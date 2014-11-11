1bbf9a9f83e8bd16390b0021cc8ec14721a92dc5
Andre Aboulian <aandre@mit.edu>
2014-11-11 01:06:47 -0800

    Shift API spec
    (incomplete)
    Shift Object Specification
    GET query filters
    POST /shifts/

--- routes/shifts.js
+++ routes/shifts.js
@@ -19,52 +19,84 @@
 
  // -----------------------
 
-//  Shift Object (definition)
+/**
+ * Shift Object Specification
+ *
+ * Shift: {
+ *   assignee: Employee,
+ *   claimant: Employee || null,
+ *   schedule: Schedule,
+ *   position: Position,
+ *   day: [0,6],
+ *   startTime: [0, 1439],
+ *   endTime: [0, 1439],
+ *   startDate: Date,
+ *   endDate: Date || null,
+ *   trading: Boolean
+ * }
+ */
+
+/**
+ * GET /shifts
+ *
+ * Description: Retrieve specified shifts within the schedule.
+ *
+ * Permissions: An employee within a schedule may retrieve information about any shift within that schedule.
+ *
+ * Query Params:
+ *   startDate - shifts occurring after or on Date
+ *   endDate - shifts occurring before or on Date
+ *   trading - include shifts currently offered for trading (options: 0 or 1, default: Any)
+ *   day - day of the week (options: [0, 6] , default: Any)
+ *   assignee - Employee assigned to shift
+ *   claimant - Employee claiming shift
+ *
+ * Response: {
+ *   shifts: Shift[]
+ * }
+ *
+ */
 
 /**
  * POST /shifts
  *
  * Description: Create a new shift.
  *
+ * Permissions: Schedule's owner (employer) may create shift with any employees and positions within their own schedule.
+ *
  * Request: {
- *  shift: Shift
+ *   shift: Shift
  * }
  *
- */
-
-/**
- * GET /shifts
- *
- * Description:
- *
- * Query Params:
- *  startDate
- *  endDate
- *  offered
- *  day
- *  employee
- *
+ * Response: {
+ *   shift: Shift
+ * }
  *
  */
 
 /**
  * GET /shifts/:id
  *
- * Description:
+ * Description: Retrieves specified shift.
+ *
+ * Permissions: User must be an employee within the schedule.
  *
  * Path Params:
- *  id
+ *   id - Shift identifier
  *
- * Query Args:
+ * Response: {
+ *   shift: Shift
+ * }
+ *
  */
 
 /**
- * PATCH /shifts/:id
+ * POST /shifts/:id
  *
  * Description:
  *
  * Path Params:
- *  id
+ *   id - Shift identifier
  *
  * Query Args:
  *
@@ -74,6 +106,20 @@
  *
  * Response: {
  *
+ * }
+ *
+ */
+
+ /**
+ * GET /shifts/:id/:date
+ *
+ * Description: Retrieves specified shift.
+ *
+ * Path Params:
+ *   id - Shift identifier
+ *
+ * Response: {
+ *   shift: Shift
  * }
  *
  */
