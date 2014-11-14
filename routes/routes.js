88b60fa0466146f1bc7a17e747490f91cdb530ef
Elliott Marquez <marquez@mit.edu>
2014-11-14 14:30:53 -0500

    Created a login template

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('auth', {authType:"login"});
+    res.render('auth', {formType:"login"});
 });
 
 router.get('/signup', function(req, res) {
-    res.render('auth', {authType:"employerSignup"});
+    res.render('auth', {formType:"employerSignup"});
+});
+
+router.get('/signupee', function(req, res) {
+    res.render('auth', {formType:"employeeSignup"});
 });
 
 module.exports = router;
