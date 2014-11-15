ad07909693a23bf9e46d3ed32f5c44b2b9de3b5a
Andre Aboulian <aandre@mit.edu>
2014-11-15 18:50:38 -0500

    User API specifications

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,86 @@
 var express = require('express');
 var router = express.Router();
+var Employee = require('../models/employee');
+var Schedule = require('../models/schedule');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
-});
+/**
+ * API Specification Authors: aandre@mit.edu, gendron@mit.edu
+ */
 
-module.exports = router;
+/**
+ * Employee Object Specification
+ *
+ * Employee: {
+ *   _id: EmployeeID,
+ *   firstName: String,
+ *   lastName: String.
+ *   email: String
+ * }
+ */
+
+/**
+ * POST /users/employers/
+ *
+ * Description: Create a new employer account with a schedule.
+ *
+ * Permissions: Any unauthenticated user.
+ *
+ * Request: {
+ *   schedule_name: String,
+ *   first_name: String,
+ *   last_name: String,
+ *   email: Email,
+ *   password: String
+ * }
+ *
+ * Error:
+ *   400 - Validation error.
+ *
+ */
+
+ /**
+ * POST /users/employees/
+ *
+ * Description: Creates an employee wihtin the schedule, generates password, and sends email notification.
+ *
+ * Permissions: An employer may create an employee within the schedule.
+ *
+ * Request: {
+ *   first_name: String,
+ *   last_name: String,
+ *   email: Email
+ * }
+ *
+ * Error:
+ *   400 - Validation error.
+ *
+ */
+
+/**
+ * GET /users/employees/
+ *
+ * Description: Retrieves all employees within the schedule.
+ *
+ * Permissions: Employers only.
+ *
+ * Response: {
+ *   employees: Employee[]
+ * }
+ *
+ */
+
+ /**
+ * GET /users/:id
+ *
+ * Description: Retrieves information about specified user.
+ *
+ * Path Params:
+ *   id - EmployeeID
+ *
+ * Permissions: Employers only.
+ *
+ * Response: {
+ *   employee: Employee
+ * }
+ *
+ */
