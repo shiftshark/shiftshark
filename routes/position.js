fe4830e48a43c40e95f100654ec3b665e428048f
Andre Aboulian <aandre@mit.edu>
2014-11-15 01:02:50 -0500

    Position API specs

---
+++ routes/position.js
@@ -0,0 +1,94 @@
+/**
+ * API Specification Author: aandre@mit.edu
+ */
+
+/**
+ * Position Object Specification
+ *
+ * Avail: {
+ *   _id: PositionID,
+ *   name: String
+ * }
+ */
+
+/**
+ * GET /positions/
+ *
+ * Description: Retrieves all positions.
+ *
+ * Permissions: All users can retrieve positions.
+ *
+ * Response: {
+ *   positions: Position[]
+ * }
+ *
+ */
+
+ /**
+ * POST /positions/
+ *
+ * Description: Create a new position.
+ *
+ * Permissions: Employers only.
+ *
+ * Request: {
+ *   position: Position
+ * }
+ *
+ * Response: {
+ *   position: Position
+ * }
+ *
+ */
+
+ /**
+ * GET /positions/:id
+ *
+ * Description: Retrieve the specific position.
+ *
+ * Permissions: All users can retrieve positions.
+ *
+ * Path Params:
+ *   id - Availibility identifier
+ *
+ * Response: {
+ *   position: Position
+ * }
+ *
+ */
+
+ /**
+ * PUT /positions/:id
+ *
+ * Description: Modify the specified position.
+ *
+ * Permissions: Employers only.
+ *
+ * Path Params:
+ *   id - Availibility identifier
+ *
+ * Request: {
+ *   position: Position
+ * }
+ *
+ * Response: {
+ *   position: Position
+ * }
+ *
+ */
+
+ /**
+ * DELETE /positions/:id
+ *
+ * Description: Delete specified position and associated shifts.
+ *
+ * Permissions: Employers only.
+ *
+ * Path Params:
+ *   id - Availibility identifier
+ *
+ * Response: {
+ *   PositionId: PositionID
+ * }
+ *
+ */
