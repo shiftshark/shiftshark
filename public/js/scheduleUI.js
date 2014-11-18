29a00822779a83ca38d585778f96e8b86ad6bf71
Elliott Marquez <marquez@mit.edu>
2014-11-18 10:29:51 -0500

    Edit Form Defaults

--- public/js/scheduleUI.js
+++ public/js/scheduleUI.js
@@ -11,7 +11,27 @@ $(document).on('click', '.cell', function(evt) {
     var startDate = new Date(shift.startDate);
     var endDate = new Date(shift.endDate);
 
-    // var startMonth = 
+    var startMonth = startDate.getMonth()+1;
+    var startDay   = startDate.getDate();
+    var startYear  = startDate.getFullYear();
+
+    var endMonth = endDate.getMonth()+1;
+    var endDay   = endDate.getDate();
+    var endYear  = endDate.getFullYear();
+
+    $('.modify.form [name="start-month"]').parent().dropdown('set value', startMonth);    
+    $('.modify.form [name="start-month"]').parent().dropdown('set selected', startMonth);    
+    $('.modify.form [name="start-day"]').parent().dropdown('set value', startDay);    
+    $('.modify.form [name="start-day"]').parent().dropdown('set selected', startDay);    
+    $('.modify.form [name="start-year"]').parent().dropdown('set value', startYear);    
+    $('.modify.form [name="start-year"]').parent().dropdown('set selected', startYear);    
+
+    $('.modify.form [name="end-month"]').parent().dropdown('set value', endMonth);    
+    $('.modify.form [name="end-month"]').parent().dropdown('set selected', endMonth);    
+    $('.modify.form [name="end-day"]').parent().dropdown('set value', endDay);    
+    $('.modify.form [name="end-day"]').parent().dropdown('set selected', endDay);    
+    $('.modify.form [name="end-year"]').parent().dropdown('set value', endYear);    
+    $('.modify.form [name="end-year"]').parent().dropdown('set selected', endYear);    
 });
 
 var changeColor = function(shift, color){
