0f8d91cc42ee6486e2028b618bb7de8adeeef6f5
Michael Belland <michaelbelland@Michaels-MacBook-Air-2.local>
2014-11-18 02:27:45 -0500

    finally starting to make good progress

--- public/js/scheduleUI.js
+++ public/js/scheduleUI.js
@@ -1,6 +1,27 @@
+/*
+ *  6.170 Schedule UI interface and Library
+ *  Lead Author: Michael Belland
+ *
+ */
+
 $(document).on('click', '.cell', function(evt) {
 	console.log(evt.target);
-	console.log(evt.target[0]);
 	evt.preventDefault();
 	alert("hello");
 });
+
+var changeColor = function(shiftId){
+
+}
+
+var addShift = function(shiftId){
+	
+}
+
+var deleteShift = function(shiftId){
+	//$("something").find("#"+shiftId);
+}
+
+var createShift = function(shiftId){
+	
+}
