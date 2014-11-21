f7c06a1e688e670a1981fd2b59bd406a439aa7ea
Andre Aboulian <aandre@mit.edu>
2014-11-21 05:30:57 -0500

    position add, contains, remove

--- public/js/timetable.js
+++ public/js/timetable.js
@@ -18,17 +18,13 @@
  * }
  */
 
- /**
- * GET /positions/
+/**
+ * Position Object Specification
  *
- * Description: Retrieves all positions.
- *
- * Permissions: All users can retrieve positions in their schedule.
- *
- * Response: {
- *   positions: Position[]
+ * Position: {
+ *   _id: PositionID,
+ *   name: String
  * }
- *
  */
 
 /**
@@ -47,10 +43,13 @@
  *   t.someFunction();
  */
 function Timetable (date) {
-  // create HTML table within container
-  var table = $(document.createElement('table'));
+  // create HTML table and initialize
+  var table = create_elem('table');
   table.addClass('timetable');
-  init_table(table);
+  init_table(table);  // construct header
+
+  var time_start = 8, time_end = 18;
+  hide_hours_left(time_start); hide_hours_right(time_end);
 
   /**
    * Function: shift_add_update
@@ -99,7 +98,10 @@ function Timetable (date) {
    *   position - Position
    */
   table.position_add_update = function (position) {
+    var row = position_row(position);
+    table.append(row);
 
+    hide_hours_left(time_start); hide_hours_right(time_end);
   };
 
   /**
@@ -111,7 +113,7 @@ function Timetable (date) {
    *   positionID - positionID: exists in table, else ignored
    */
   table.position_remove = function (positionID) {
-
+    $('.timetable tr[positionID=' + positionID + ']').remove();
   };
 
   /**
@@ -125,15 +127,15 @@ function Timetable (date) {
    * Returns: (Boolean) true if position appears on timetable
    */
   table.position_contains = function (positionID) {
-
+    return $('.timetable tr[positionID=' + positionID + ']').length !== 0;
   };
 
   return table;
 }
 
-//////////////////////
-// Helper Functions //
-//////////////////////
+///////////////////////
+// Utility Functions //
+///////////////////////
 
 /**
  * Function: date_equals
@@ -150,6 +152,121 @@ function date_equals (a, b) {
   return a.toDateString() === b.toDateString();
 }
 
-function init_table (table_sel) {
+/**
+ * Function: create_elem
+ *
+ * Creates a jQuery DOM element.
+ *
+ * Parameters:
+ *   elem - HTML tag
+ *
+ * Returns: jQuery DOM element
+ */
+function create_elem (elem) {
+  return $(document.createElement(elem));
+}
 
+function hide_hours_left (hour) {
+  $('.timetable').ready(function() {
+    for (var h = hour - 1; h >= 0; h--) {
+      var hour_col = $('.timetable td[hour='+ h + ']');
+      if (hour_col.first().css('display') !== 'none') {
+        hour_col.css('display', 'none');
+      } else break;
+    }
+  });
+}
+
+function hide_hours_right (hour) {
+  $('.timetable').ready(function() {
+    for (var h = hour + 1; h < 24; h++) {
+      var hour_col = $('.timetable td[hour='+ h + ']');
+      if (hour_col.first().css('display') !== 'none') {
+        hour_col.css('display', 'none');
+      } else break;
+    }
+  });
+}
+
+function extend_hours_left (hour) {
+  $('.timetable').ready(function() {
+    for (var h = hour; h < 24; h++) {
+      var hour_col = $('.timetable td[hour='+ h + ']');
+      if (hour_col.first().css('display') === 'none') {
+        hour_col.css('display', '');
+      } else break;
+    }
+  });
+}
+
+function extend_hours_right (hour) {
+  $('.timetable').ready(function() {
+    for (var h = hour; h >= 0; h--) {
+      var hour_col = $('.timetable td[hour='+ h + ']');
+      if (hour_col.first().css('display') === 'none') {
+        hour_col.css('display', '');
+      } else break;
+    }
+  });
+}
+
+///////////////////////
+// Helper Procedures //
+///////////////////////
+
+function init_table (table) {
+  // hide table during consturction
+  table.hide(0);
+
+  // construct time header
+  header = create_elem('tr').addClass('header-row');
+  header.append(create_elem('td').css('border','none')); // empty square
+  table.append(header);
+
+  for (var h = 0; h < 24; h++) {
+    var timestring = "";
+    // hour
+    if (h % 12 == 0)
+      timestring = "12:00";
+    else
+      timestring = String(h % 12) + ":00";
+    // meridian
+    if (h < 12)
+      timestring += " am";
+    else
+      timestring += " pm";
+    // time heading
+    var heading = create_elem('td');
+    heading.attr('colspan', '4');
+    heading.addClass('label-time');
+    heading.attr('hour', h);
+    heading.html(timestring);
+    header.append(heading);
+  }
+
+  // show table once created
+  table.show(0);
+}
+
+function position_row (position) {
+  // create row with ID
+  var row = create_elem('tr');
+  row.attr('positionID', position._id);
+
+  // position name/label on left
+  var label = create_elem('td');
+  label.addClass('label-position');
+  label.html(position.name);
+  row.append(label);
+
+  // crete 15-minute blocks
+  for (var h = 0; h < 24; h++) {
+    for (var m = 0; m < 4; m++) {
+      var block = create_elem('td').addClass('time-block');
+      block.attr({hour: h, quarter: m});
+      row.append(block);
+    }
+  }
+
+  return row;
 }
